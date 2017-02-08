e281bbc74d0935f0a15ee0e0803621325fb80b0a
Sergej Keselj <keshelj@hotmail.com>
2017-02-08 17:42:24 +0100

    run server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,19 +7,22 @@ python3 manage.py migrate app              # Apply database migrations
 #echo Collect static files
 #python3 manage.py collectstatic --noinput  # Collect static files
 
+#run server
+python3 manage.py runserver
+
 # Prepare log files and start outputting logs to stdout
-touch /srv/logs/gunicorn.log
-touch /srv/logs/access.log
-tail -n 0 -f /srv/logs/*.log &
+#touch /srv/logs/gunicorn.log
+#touch /srv/logs/access.log
+#tail -n 0 -f /srv/logs/*.log &
 
 # Start Gunicorn processes
-echo Starting Gunicorn.
-exec gunicorn apps.wsgi:application \
-    --name uks-project \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --log-level=info \
-    --log-file=/srv/logs/gunicorn.log \
-    --access-logfile=/srv/logs/access.log \
-    "$@"
+#echo Starting Gunicorn.
+#exec gunicorn apps.wsgi:application \
+#    --name uks-project \
+#    --bind 0.0.0.0:8000 \
+#    --workers 3 \
+#    --log-level=info \
+#    --log-file=/srv/logs/gunicorn.log \
+#    --access-logfile=/srv/logs/access.log \
+#    "$@"
 
