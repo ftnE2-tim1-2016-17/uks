6c034e0ff238b903d4577c1dafe6ac114e3ae0d7
Sergej Keselj <keshelj@hotmail.com>
2017-03-08 17:02:07 +0100

    izmene za docker-compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo Prepare database migrations           # Create database migrations
-python3 manage.py makemigrations app
+#python3 manage.py makemigrations app
 echo Apply database migrations
-python3 manage.py migrate app              # Apply database migrations
+#python3 manage.py migrate app              # Apply database migrations
 
 #echo Collect static files
 #python3 manage.py collectstatic --noinput  # Collect static files
@@ -17,12 +17,12 @@ python3 manage.py migrate app              # Apply database migrations
 #tail -n 0 -f /srv/logs/*.log &
 
 # Start Gunicorn processes
-echo Starting Gunicorn.
-exec gunicorn wsgi:application \
-    --name uks-project \
-    --bind 0.0.0.0:8000 \
-    --workers 1 \
-    "$@"
+#echo Starting Gunicorn.
+#exec gunicorn wsgi:application \
+#    --name uks-project \
+#    --bind 0.0.0.0:8000 \
+#    --workers 1 \
+#    "$@"
 #    --log-level=info \
 #    --log-file=/srv/logs/gunicorn.log \
 #    --access-logfile=/srv/logs/access.log \
